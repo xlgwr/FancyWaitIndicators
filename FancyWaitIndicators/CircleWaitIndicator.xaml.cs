e316f01701a3cab37bd098bfa27d8451108ae61b
gellerda <dennisgeller@gmail.com>
2019-10-15 23:45:45 +0300

    Добавьте файлы проекта.

---
+++ FancyWaitIndicators/CircleWaitIndicator.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FancyWaitIndicators
+{
+    /// <summary>
+    /// Логика взаимодействия для CircleWaitIndicator.xaml
+    /// </summary>
+    public partial class CircleWaitIndicator : UserControl
+    {
+        public CircleWaitIndicator()
+        {
+            InitializeComponent();
+        }
+        //------------------------------------------------------------------------------------------------------------------------------------
+        public bool IsRunning
+        {
+            get { return (bool)GetValue(IsRunningProperty); }
+            set { SetValue(IsRunningProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsRunning.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsRunningProperty =
+            DependencyProperty.Register("IsRunning", typeof(bool), typeof(CircleWaitIndicator), new PropertyMetadata(false));
+        //------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------
+    }
+}
